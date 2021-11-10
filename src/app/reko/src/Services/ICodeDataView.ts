dbb77326a810e2808ed48ac1b1eb49fe2566ed0e
Stefano Moioli <smxdev4@gmail.com>
2021-11-11 00:39:21 +0100

    Cleanup

--- src/app/reko/src/Services/ICodeDataView.ts
+++ src/app/reko/src/Services/ICodeDataView.ts
@@ -1,7 +1,3 @@
-export declare module ICodeDataView {
-	export const ServiceID = "ICodeDataView"
-}
-
 export interface ICodeDataView {
 	showProcedure(procAddr: string): void
 }
--- src/app/reko/src/Services/IServiceContainer.ts
+++ src/app/reko/src/Services/IServiceContainer.ts
@@ -1,7 +1,3 @@
-export declare namespace IServiceContainer {
-	export const ServiceID = "IServiceContainer"
-}
-
 export interface IServiceContainer {
 	removeService<T extends object>(serviceId: string): boolean
 	addService<T extends object>(serviceId: string, serviceInstance: T): void
